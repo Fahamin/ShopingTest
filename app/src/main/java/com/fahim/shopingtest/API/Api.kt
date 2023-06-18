29c86865e399900ead1866ff6d54d567330985f4
Fahamin <fahamin00@gmail.com>
2023-06-18 15:17:25 +0600

    retrofit test

--- app/src/main/java/com/fahim/shopingtest/API/Api.kt
+++ app/src/main/java/com/fahim/shopingtest/API/Api.kt
@@ -1,11 +1,17 @@
 package com.fahim.shopingtest.API
 
 import com.fahim.shopingtest.Model.*
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.FieldMap
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface Api {
 
@@ -20,5 +26,32 @@ interface Api {
 
     @POST("user/{userid}/addtocart")
     suspend fun addToChart(@Path("userid") uid: String)
+
+
+    @Headers("Content-Type: application/json; charset=utf-8")
+    @FormUrlEncoded
+    @POST("/api/ApiCheckUser/ApiCheckUser")
+    suspend fun getLoginInfo(
+        @FieldMap param: HashMap<String?, String?>?,
+        @Header("Authorization") authorization: String?
+    ): Response<HomePageImageModel>
+
+
+    @Headers("Content-Type: application/json; charset=utf-8")
+    @GET("/api/ApiSyncPromotation/ApiGetTradePromotionNew?")
+    suspend fun getOfferList(
+        @Header("Authorization") authorization: String?,
+        @Query("dbid") id: String?,
+        @Query("psrid") psrid: String?,
+        @Query("token") token: String?
+    ):Response<HomePageImageModel>
+
+    @Headers("Content-Type: application/json; charset=utf-8")
+    @POST("/api/ApiSyncUpNewOutlet/ApiSyncEdsOutlet")
+    suspend fun sendEditOutletSync(
+        @Header("Authorization") authorization: String?,
+        @Body body: String?
+    ): Response<HomePageImageModel>
+
 
 }
